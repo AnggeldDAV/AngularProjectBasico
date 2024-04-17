732766a0f27936e61fbf81781eafc58e1bede8dc
AnggeldDAV <anggeld.adav@gmail.com>
2024-04-17 14:51:24 +0200

    ejercicios angular

--- AngularProjectBasico/src/app/app.component.ts
+++ AngularProjectBasico/src/app/app.component.ts
@@ -8,7 +8,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   count = 0;
   mensaje = '';
+  entrada: string = "";
+  numero1: number = 0;
+  numero2: number = 0;
+  palindromo: string = "";
+  textoMedido: string = "";
+  guiones = "";
   dameMensaje() {
     return `${this.mensaje}  ${this.count}`;
+  }
+  resultado() {
+    return Number(this.numero1) + Number(this.numero2);
+  }
+  palindromoreverse() {
+    return this.palindromo.split("").reverse().join("");
+  }
+  medirTexto() {
+    let longitud = this.textoMedido.length;
+    this.guiones = "";
+    for (let i = 0; i < longitud; i++) {
+      this.guiones += "-";
+    }
+    return longitud;
   }
 }
